2f9a753d6c46f5f98f159c67ba5bb723ea2ebc69
Ana <anacarolgbn@icloud.com>
2022-03-21 11:12:54 -0300

    Criação do Projeto

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using RPG.SRC.Entities;
+using System;
+
+namespace RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Knight arus = new("Arus", 23, "knight");
+            Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+
+            Console.WriteLine(arus.Attack());
+            Console.WriteLine(wizard.Attack());
+            Console.WriteLine(wizard.Attack(5));
+            Console.WriteLine(wizard.Attack(8));
+        }
+    }
+}
